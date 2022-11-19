815bf222855597e72bee7f864100aefe08b8c531
Jamesson Faria <jamessonjr@gmail.com>
2022-11-19 14:01:30 -0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        color: Colors.black,
+        alignment: Alignment.center,
+        child: const Text("Ola mundo"),
+      ),
+    );
+  }
+
+
+
+}
