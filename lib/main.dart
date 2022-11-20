6688cc436dc24566a3e48172c1d3d97dac09d080
Jamesson Faria <jamessonjr@gmail.com>
2022-11-19 23:04:44 -0300

    Adicionando os botões

--- lib/main.dart
+++ lib/main.dart
@@ -18,14 +18,22 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  void decrement() {
+    print("Decrement");
+  }
+
+  void increment() {
+    print("Increment");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.red,
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            Text(
+          children: [
+            const Text(
               "Pode entrar!",
               style: TextStyle(
                 fontSize: 30,
@@ -33,12 +41,32 @@ class HomePage extends StatelessWidget {
                 fontWeight: FontWeight.w800,
               ),
             ),
-            Text(
+            const Text(
               "0",
-              style: TextStyle(
-                fontSize: 100,
-                color: Colors.white
-              ),
+              style: TextStyle(fontSize: 100, color: Colors.white),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: decrement,
+                  child: const Text(
+                    "Saiu",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16,
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: increment,
+                  child: const Text("Entrou",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 16,
+                      )),
+                )
+              ],
             ),
           ],
         ));
